7ac7579954a34806fa515a2f3d8faa3cb8ea7f31
dashWoorkZ <157441365+dashWoorkZ@users.noreply.github.com>
2024-01-22 22:07:55 -0800

    Structural Template
    Created a basic landing page with minimal CSS3 and Javascript

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// CHILD of BODY - Contains Everything
+const container = document.querySelector('.container');
+// Logo Image - Left Side of Header
+const logo = document.querySelector('.logo');
+// Brand Name in a SPAN - CSS Styled
+const brandNameTitle = document.querySelector('.brandNameTitle');
+// Login Button - Filler Item at prsent
+const login = document.querySelector('.login');
+// Sidebar of sites i found useful
+const links = document.querySelector('.links');
+// Navigation Bar for entir site
+const navLinks = document.getElementsByTagName('nav');
+// Body Last Child - last minute info
+const footer = document.getElementsByTagName('footer');
+// Main Body Content - Future Single Page Display
+const main = document.getElementsByTagName('main');
+// First Child of Main Container - Page Identity
+const header = document.getElementsByTagName('header');
